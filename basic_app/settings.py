11803529be44f279ed7f3303554ae90595b370d3
kumkum <kumkum@snakescript.com>
2024-07-18 17:19:21 +0530

    testing

--- basic_app/settings.py
+++ basic_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^689*jglme!yin8_x2zp*um!$(1s$c+j-#9h84puq)0!nzu1(y'
+SECRET_KEY = 'kum'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
